3877361134a9a0e94bf58c5e60c7662e0201ec15
aaronkaz <aaronmichaelmusic@gmail.com>
2016-09-22 12:30:53 -0400

    basic test setup

--- src/CloudSearchQuery.php
+++ src/CloudSearchQuery.php
@@ -1,1 +1,16 @@
-<?php 
+<?php
+
+namespace AaronKaz\CloudSearchQuery;
+
+class CloudSearchQuery
+{
+    public function __construct()
+    {
+
+    }
+
+    public function foo()
+    {
+        return "Hello World";
+    }
+}
